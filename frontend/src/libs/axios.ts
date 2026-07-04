a9c783be0b774197c0a830cd579731308bc7c3be
Maitry Gala <maitryg1004@gmail.com>
2026-07-04 19:27:34 +0530

    edited env

--- frontend/src/libs/axios.ts
+++ frontend/src/libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: " https://second-brain-n95c.onrender.com/api/v1",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
